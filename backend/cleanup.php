760930a63576fd54884d573167f3d5ea57edf6a1
Wendy Gervais <wendygervais@Wendys-MacBook-Pro.local>
2025-11-17 10:56:08 +0100

    flush all

--- backend/cleanup.php
+++ backend/cleanup.php
@@ -1,16 +1,13 @@
 <?php
 $dir = __DIR__ . "/images";
-$days = 7;
 
-$now = time();
 $files = glob("$dir/*");
 
 foreach ($files as $file) {
     if (is_file($file)) {
-        if ($now - filemtime($file) > ($days * 86400)) {
             unlink($file);
-        }
     }
 }
 
-echo "Cleanup done.";
+
+echo "Toutes les images ont été effacées.";
