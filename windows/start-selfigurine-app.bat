11b7b91055184013cdf5f4005fd7567d75a27256
Wendy Gervais <wendy.gervais@reciproque.com>
2026-04-17 16:45:34 +0200

    update batch

--- windows/start-selfigurine-app.bat
+++ windows/start-selfigurine-app.bat
@@ -4,10 +4,9 @@ setlocal EnableDelayedExpansion
 set "DISPOSITIF=%~1"
 set "URL=http://localhost:5173/"
 
-@REM taskkill /im explorer.exe /f
+taskkill /im explorer.exe /f
 
 net start "TabletInputService"
 
-timeout /t 2 >nul
-@REM start chrome.exe --kiosk --overscroll-history-navigation=0 --disable-pinch --noerrors --disable-session-crashed-bubble --disable-infobars --window-position=0,0 --window-size=720,120 --noerrdialogs "%URL%"
-start chrome.exe --disable-pinch --noerrors --disable-session-crashed-bubble --disable-infobars --window-size=880,1700 --noerrdialogs "%URL%"
+timeout /t 5 >nul
+start chrome.exe --kiosk --overscroll-history-navigation=0 --disable-pinch --noerrors --disable-session-crashed-bubble --disable-infobars --window-position=0,0 --window-size=720,120 --noerrdialogs "%URL%"
